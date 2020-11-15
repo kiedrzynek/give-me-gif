1f705621d7e5f164627ce96513c8c9753beb94a4
Kamila Kiedrzynek <kamilakiedrzynek@gmail.com>
2020-11-15 16:22:57 +0100

    Get a random gif from GIPHY API's endpoint

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-    console.log('ok');
+    $.get(
+        'https://api.giphy.com/v1/gifs/random',
+        {
+            api_key: 'udyq3h2fMZG5YRkgWF3Q2f0t61pUNnSc',
+            rating: 'g'
+        }
+    ).done(function(response) {
+        let data = response.data;
+        $('body').append(`<img src=${data.images.downsized.url} />`);
+    }).fail(function(data) {
+        let faildata = data.responseJSON.meta.msg;
+        console.log(faildata);
+        $('body').append(`<div>Sorry, something went wrong. Gif it another try :)</div>`);
+    })
 });
