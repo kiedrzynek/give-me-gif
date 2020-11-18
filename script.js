2d03a122a13b44ac29f0f2d768b961bd5d4a8110
Kamila Kiedrzynek <kamilakiedrzynek@gmail.com>
2020-11-18 21:02:22 +0100

    Add input, style it and search gif by its value

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
-    $.get(
-        'https://api.giphy.com/v1/gifs/random',
-        {
-            api_key: 'udyq3h2fMZG5YRkgWF3Q2f0t61pUNnSc',
-            rating: 'g',
-            tag: 'cat'
-        }
-    ).done(function(response) {
-        let data = response.data;
-        $('main').append(`<img class="gif" src=${data.images.downsized.url} />`);
-    }).fail(function(data) {
-        let faildata = data.responseJSON.meta.msg;
-        console.log(faildata);
-        $('body').append(`<div>Sorry, something went wrong. Gif it another try :)</div>`);
-    })
+
+    $(document).on('click', 'button', function() {
+        let $tagValue = $('input').val();
+
+        $.get(
+            'https://api.giphy.com/v1/gifs/random',
+            {
+                api_key: 'udyq3h2fMZG5YRkgWF3Q2f0t61pUNnSc',
+                rating: 'g',
+                tag: $tagValue
+            }
+        ).done(function(response) {
+            let data = response.data;
+            $('#gif-container')
+            .empty()
+            .append(`<img class="gif" src=${data.images.downsized.url} />`);
+        }).fail(function(data) {
+            let faildata = data.responseJSON.meta.msg;
+            console.log(faildata);
+            $('body').append(`<div>Sorry, something went wrong. Gif it another try :)</div>`);
+        });
+    });
 });
